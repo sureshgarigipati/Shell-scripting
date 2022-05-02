7a685f1c33a88bf33027b3fb89fb0acab860d70f
sureshgarigipati <sureshbabu7569@gmail.com>
2022-05-02 11:07:26 +0530

    update

---
+++ arg.sh
@@ -0,0 +1,8 @@
+echo " Arguments Total : $# "
+echo " script name : $0 "
+echo " script 1st argement : $1 "
+echo " script 2nd argument : $2 "
+echo " script 3rd argument : $3 "
+echo " list of all aruments : * $* "
+echo " list of all arguments : @ $@ "
+echo " pid of shell : $$ "
